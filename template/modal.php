3f52d923728508d57d3d3252a148bdd344a095fe
WahyuLamani <wahyu@elektro.polimdo.ac.id>
2020-10-21 20:47:52 +0800

    fix bug halaman access

--- template/modal.php
+++ template/modal.php
@@ -1,3 +1,7 @@
+<?php
+if (!defined('BASEPATH')) exit('<h1>Error 404 Not Found !</h1>');
+?>
+
 <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
